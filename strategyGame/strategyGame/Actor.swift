b9cab35b320af56457adc7237a7c38a90376f04f
Edgar Sgroi <edgarsgroi@Edgars-MacBook-Pro-3.local>
2019-10-16 21:09:57 -0300

    First movimentation mechanics. Not concluded

--- strategyGame/strategyGame/Actor.swift
+++ strategyGame/strategyGame/Actor.swift
@@ -9,32 +9,30 @@
 import Foundation
 import SpriteKit
 
-class Character: SKSpriteNode {
+class Actor: SKSpriteNode {
     
     var coord: (Int, Int)
-    var spriteAtlas: SKTextureAtlas
-    var state: StateMachine
-    private let movement: Int
+    var sprite: SKTexture
+    var state: State
+    var movement: Int
     var damage: Int
     var health: Int
     var attackRange: Int
-    var breadcrumbs: [Tile]
+    var breadcrumbs: [Tile] = [Tile]()
+    var wholeNumberValue: Float?
     
-    
-    init(name: String, movement: Int, coord: (Int,Int), spriteAtlas: SKTextureAtlas, state: StateMachine, damage: Int, health: Int, attackRange: Int, breadcrumbs: [Tile]) {
-        super.init()
-        self.name = name
+    init(name: String, movement: Int, coord: (Int, Int), sprite: SKTexture, state: State, damage: Int, health: Int, attackRange: Int) {
+//        self.name = name
         self.movement = movement
-        self.coord = coord
-        self.spriteAtlas = spriteAtlas
+        self.coord = (coord.0, coord.1)
+        self.sprite = sprite
         self.state = state
         self.damage = damage
         self.health = health
         self.attackRange = attackRange
-        self.breadcrumbs = breadcrumbs
+        super.init(texture: SKTexture(imageNamed: "OysterVolcano"), color: UIColor.clear, size: SKTexture(imageNamed: "OysterVolcano").size())
+        self.isUserInteractionEnabled = false
     }
-    
-    
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -46,7 +44,7 @@ class Character: SKSpriteNode {
     
     func specialAttack() {}
     
-    func takeDamage(damage: Int){
+    func takeDamage(damage: Int) {
         self.health -= damage
         if (self.health <= 0) {
             //change state to dead
